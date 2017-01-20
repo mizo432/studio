1643a81f12bc5641a191327ee07abe24c150c40a
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-01-20 16:22:35 +0900

    ビルドを通るように修正した

---
+++ web/src/test/java/jp/or/venuspj/studo/StudioApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.or.venuspj.studo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(jp.or.venuspj.studo.StudioApplicationTests.class, args);
+    }
+
+}
