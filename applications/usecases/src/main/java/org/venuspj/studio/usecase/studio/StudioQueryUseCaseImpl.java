bf4ae4bfa7e0d68e532e4ee39dbe8f04db7949a9
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-06-22 20:10:54 +0900

    一部パッケージを移動した

--- applications/usecases/src/main/java/org/venuspj/studio/usecase/studio/StudioQueryUseCaseImpl.java
+++ applications/usecases/src/main/java/org/venuspj/studio/usecase/studio/StudioQueryUseCaseImpl.java
@@ -1,8 +1,8 @@
-package org.venuspj.studio.usecases.studio;
+package org.venuspj.studio.usecase.studio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.venuspj.studio.core.usecases.studio.StudioQueryUseCase;
 import org.venuspj.studio.service.StudioServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class StudioQueryUseCaseImpl implements StudioQueryUseCase {
 
