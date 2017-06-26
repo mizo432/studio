c59d4bc90b07e541d8e71cd8d8ece32215a3ebd7
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-06-26 18:37:18 +0900

    AddressStringテスト強化

--- models/core/src/test/java/org/venuspj/studio/core/fundamentals/address/AddressStringTest.java
+++ models/core/src/test/java/org/venuspj/studio/core/fundamentals/address/AddressStringTest.java
@@ -12,7 +12,7 @@ public class AddressStringTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(AddressStringTest.class);
 
     public static AddressString createDummy() {
-        return AddressString.emptyAddressString();
+        return new AddressString("ADDRESS_STRING");
     }
 
     @Test
@@ -26,6 +26,15 @@ public class AddressStringTest {
 
     @Test
     public void testToString2() throws Exception {
+        AddressString target = AddressString.emptyAddressString();
+        String actual = target.toString();
+        assertThat(actual)
+                .isNotNull();
+        LOGGER.debug("actual:" + target.toString());
+    }
+
+    @Test
+    public void testToString3() throws Exception {
         AddressString target = createDummy();
         String actual = target.toString();
         assertThat(actual)
@@ -33,5 +42,14 @@ public class AddressStringTest {
         LOGGER.debug("actual:" + target.toString());
     }
 
+    @Test
+    public void asText() throws Exception {
+        AddressString target = createDummy();
+        String actual = target.asText();
+        assertThat(actual)
+                .isNotNull()
+                .isEqualTo("ADDRESS_STRING");
+        LOGGER.debug("actual:" + actual);
+    }
 
 }
