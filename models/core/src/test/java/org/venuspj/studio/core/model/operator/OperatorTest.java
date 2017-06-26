ea94b0f4744e3f48c39da0ef58e8851f12b57534
Mizoguchi <kenichi.mizoguchi@asoview.co.jp>
2017-06-26 18:50:04 +0900

    Operatorテスト強化

---
+++ models/core/src/test/java/org/venuspj/studio/core/model/operator/OperatorTest.java
@@ -0,0 +1,36 @@
+package org.venuspj.studio.core.model.operator;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class OperatorTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OperatorTest.class);
+
+    public static Operator createDummy() {
+        return new Operator(OperatorIdTest.createDummy());
+    }
+
+    @Test
+    public void toString01() throws Exception {
+        Operator target = new Operator();
+        assertThat(target)
+                .isNotNull();
+        assertThat(target.toString())
+                .isNotNull();
+        LOGGER.debug("actual:" + target.toString());
+    }
+
+    @Test
+    public void toString02() throws Exception {
+        Operator target = createDummy();
+        assertThat(target)
+                .isNotNull();
+        assertThat(target.toString())
+                .isNotNull();
+        LOGGER.debug("actual:" + target.toString());
+    }
+
+}
