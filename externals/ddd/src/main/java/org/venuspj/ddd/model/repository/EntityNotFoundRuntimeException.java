fe9146249e781f42621db1359bfc1db4553cdf5d
溝口 顕一 <hachirouuemon.mizoguchi@gmail.com>
2017-07-17 17:09:28 +0900

    コンストラクターを見直した

--- externals/ddd/src/main/java/org/venuspj/ddd/model/repository/EntityNotFoundRuntimeException.java
+++ externals/ddd/src/main/java/org/venuspj/ddd/model/repository/EntityNotFoundRuntimeException.java
@@ -9,8 +9,10 @@ import org.venuspj.ddd.model.entity.EntityIdentifier;
 public class EntityNotFoundRuntimeException extends RuntimeException {
     EntityIdentifier<?> identifier;
 
-    public <T extends Entity<T>> EntityNotFoundRuntimeException(EntityIdentifier<?> identifier) {
-        super("Entity not found. identifier:" + identifier);
+    public <T extends Entity<T>> EntityNotFoundRuntimeException(EntityIdentifier<?> anIdentifier) {
+        super("Entity not found. identifier:" + anIdentifier);
+        identifier = anIdentifier;
+
     }
 
     EntityIdentifier<?> identifier() {
