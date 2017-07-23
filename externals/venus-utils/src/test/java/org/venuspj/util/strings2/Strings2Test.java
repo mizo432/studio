225a340f4fc16ae20deb61a685081a0d12cd0709
溝口 顕一 <hachirouuemon.mizoguchi@gmail.com>
2017-07-23 15:02:05 +0900

    テストを追加した

--- externals/venus-utils/src/test/java/org/venuspj/util/strings2/Strings2Test.java
+++ externals/venus-utils/src/test/java/org/venuspj/util/strings2/Strings2Test.java
@@ -5,6 +5,117 @@ import org.junit.Test;
 import static org.assertj.core.api.Java6Assertions.*;
 
 public class Strings2Test {
+    public static class IsEmptyTest {
+        @Test
+        public void test01() throws Exception {
+            String arg = "";
+            boolean actual = Strings2.isEmpty(arg);
+            assertThat(actual)
+                    .isTrue();
+
+        }
+
+        @Test
+        public void test02() throws Exception {
+            String arg = "";
+            boolean actual = Strings2.isEmpty(arg);
+            assertThat(actual)
+                    .isTrue();
+
+        }
+
+        @Test
+        public void test03() throws Exception {
+            String arg = "A";
+            boolean actual = Strings2.isEmpty(arg);
+            assertThat(actual)
+                    .isFalse();
+
+        }
+    }
+
+    public static class IsNotEmptyTest {
+        @Test
+        public void test01() throws Exception {
+            String arg = "";
+            boolean actual = Strings2.isNotEmpty(arg);
+            assertThat(actual)
+                    .isFalse();
+
+        }
+
+        @Test
+        public void test02() throws Exception {
+            String arg = "";
+            boolean actual = Strings2.isNotEmpty(arg);
+            assertThat(actual)
+                    .isFalse();
+
+        }
+
+        @Test
+        public void test03() throws Exception {
+            String arg = "A";
+            boolean actual = Strings2.isNotEmpty(arg);
+            assertThat(actual)
+                    .isTrue();
+
+        }
+    }
+
+
+    public static class IsTestTest {
+        @Test
+        public void test01() throws Exception {
+            String arg = "";
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isFalse();
+        }
+
+        @Test
+        public void test02() throws Exception {
+            String arg = "1";
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isTrue();
+        }
+
+        @Test
+        public void test03() throws Exception {
+            String arg = " 1";
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isFalse();
+        }
+
+        @Test
+        public void test04() throws Exception {
+            String arg = "3 ";
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isFalse();
+        }
+
+        @Test
+        public void test05() throws Exception {
+            String arg = String.valueOf(Integer.MIN_VALUE);
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isTrue();
+        }
+
+        @Test
+        public void test06() throws Exception {
+            String arg = String.valueOf(Integer.MAX_VALUE);
+            boolean actual = Strings2.isNumber(arg);
+            assertThat(actual)
+                    .isTrue();
+        }
+
+    }
+
+
     public static class RepeatTest {
 
         @Test
