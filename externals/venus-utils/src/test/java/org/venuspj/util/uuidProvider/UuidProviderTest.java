773d979f94a6f60668d7bcb16ebaa1f2e820c387
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-08-02 12:21:19 +0900

    UuidProviderを追加した

---
+++ externals/venus-utils/src/test/java/org/venuspj/util/uuidProvider/UuidProviderTest.java
@@ -0,0 +1,13 @@
+package org.venuspj.util.uuidProvider;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+public class UuidProviderTest {
+    @Test
+    public void randomUUID() throws Exception {
+        UUID actual = UuidProvider.randomUUID();
+        System.out.println("actual:" + actual);
+    }
+}
