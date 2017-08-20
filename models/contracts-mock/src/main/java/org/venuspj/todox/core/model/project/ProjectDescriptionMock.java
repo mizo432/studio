d22e406bd965b4b965b36dd5eae525bdfdc087b1
溝口 顕一 <hachirouuemon.mizoguchi@gmail.com>
2017-08-21 00:41:47 +0900

    モジュールを追加した

--- models/contracts-mock/src/main/java/org/venuspj/todox/core/model/project/ProjectDescriptionMock.java
+++
@@ -1,7 +0,0 @@
-package org.venuspj.todox.core.model.project;
-
-public class ProjectDescriptionMock {
-    public static ProjectDescription createDummy(ProjectMock.ProjectType aProjectType) {
-       return new ProjectDescription();
-    }
-}
--- models/contracts-mock/src/main/java/org/venuspj/todox/core/model/project/ProjectIdMock.java
+++
@@ -1,7 +0,0 @@
-package org.venuspj.todox.core.model.project;
-
-public class ProjectIdMock {
-    public static ProjectId createDummy(ProjectMock.ProjectType aProjectType) {
-        return new ProjectId(aProjectType.idValue());
-    }
-}
