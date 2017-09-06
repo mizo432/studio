96322a33c30f63d219c224c4fc7bd2684ccabde6
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-09-06 12:40:10 +0900

    findbugsのワーニングに対応した

--- externals/ddd/src/main/java/org/venuspj/ddd/model/entity/EntityBuilder.java
+++ externals/ddd/src/main/java/org/venuspj/ddd/model/entity/EntityBuilder.java
@@ -1,5 +1,6 @@
 package org.venuspj.ddd.model.entity;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.venuspj.util.builder.ObjectBuilder;
 import org.venuspj.util.objects2.Objects2;
 
@@ -15,6 +16,7 @@ public abstract class EntityBuilder<E extends Entity<E>, B extends EntityBuilder
         builder.withEntityIdentifier(vo.identifier());
     }
 
+    @SuppressFBWarnings("URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
     public B withEntityIdentifier(EntityIdentifier<E> anIdentifier) {
         if (Objects2.isNull(anIdentifier)) return getThis();
         addConfigurator(builder -> builder.identifier = anIdentifier);
