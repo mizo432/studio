ccbd29598503c70e8f64bfc7863ce222135e1ac4
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-09-25 14:15:31 +0900

    一部テーブルを追加した

---
+++ models/generic/src/main/java/org/venuspj/studio/generic/fundamentals/geographic/Latitude.java
@@ -0,0 +1,30 @@
+package org.venuspj.studio.generic.fundamentals.geographic;
+
+import org.venuspj.util.strings2.Strings2;
+
+import java.io.Serializable;
+
+import static org.venuspj.util.objects2.Objects2.toStringHelper;
+
+public class Latitude implements Serializable {
+    private String value;
+
+    public Latitude(String value) {
+        this.value = value;
+    }
+
+    public boolean isUndefined() {
+        return Strings2.isEmpty(value) || "0.0".equals(value);
+    }
+
+    public String asText(){
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return toStringHelper(this)
+                .addValue(value)
+                .toString();
+    }
+}
