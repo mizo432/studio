eed11f7503799100bb29dbcfb1aa624f37cbf9e8
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-10-10 10:51:44 +0900

    [LHS-41-73] dataSourcesのテストが通らないのを通るように修正した

--- bootstraps/web/src/main/java/org/venuspj/studio/StudioWebApplication.java
+++ bootstraps/web/src/main/java/org/venuspj/studio/StudioWebApplication.java
@@ -2,9 +2,11 @@ package org.venuspj.studio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.util.ClassUtils;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class StudioWebApplication {
     public static void main(String[] args) {
         final SpringApplication springApplication = new SpringApplication(StudioWebApplication.class);
