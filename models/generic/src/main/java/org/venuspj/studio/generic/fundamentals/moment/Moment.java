5a7523039f103d97a24766282d870fd638341ee7
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-12-06 17:49:39 +0900

    [LHS-41-73]Java warningに対応した

--- models/generic/src/main/java/org/venuspj/studio/generic/fundamentals/moment/Moment.java
+++ models/generic/src/main/java/org/venuspj/studio/generic/fundamentals/moment/Moment.java
@@ -4,6 +4,14 @@ import org.venuspj.studio.generic.fundamentals.datetime.RecordDateTime;
 import org.venuspj.studio.generic.model.ppt.party.Party;
 
 public class Moment {
+    public RecordDateTime getRecordDateTime() {
+        return recordDateTime;
+    }
+
+    public Party getParty() {
+        return party;
+    }
+
     RecordDateTime recordDateTime;
     Party party;
 
