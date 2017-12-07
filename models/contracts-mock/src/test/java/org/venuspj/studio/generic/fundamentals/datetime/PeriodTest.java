9f5431ed88dfcfc0d9a7342e1f7df2c538dc52e9
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-12-07 11:24:36 +0900

    [LHS-41-73]テストを修正した

--- models/contracts-mock/src/test/java/org/venuspj/studio/generic/fundamentals/datetime/PeriodTest.java
+++ models/contracts-mock/src/test/java/org/venuspj/studio/generic/fundamentals/datetime/PeriodTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import static org.assertj.core.api.Java6Assertions.*;
 
 public class PeriodTest {
+
     @Test
     public void startDate() throws Exception {
         Period target = new Period(new RecordDate(LocalDate.MIN), new RecordDate(LocalDate.MAX));
@@ -23,36 +24,6 @@ public class PeriodTest {
         assertThat(actual)
                 .isNotNull()
                 .isEqualTo(new RecordDate(LocalDate.MAX));
-    }
-
-    @Test
-    public void toString01() throws Exception {
-        Period target = new Period(new RecordDate(LocalDate.MIN), new RecordDate(LocalDate.MAX));
-        String actual = target.toString();
-        assertThat(actual)
-                .isNotNull()
-                .isEqualTo("Period{startDate=RecordDate{-999999999-01-01}, endDate=RecordDate{+999999999-12-31}}");
-
-    }
-
-    @Test
-    public void toString02() throws Exception {
-        Period target = new Period(null, null);
-        String actual = target.toString();
-        assertThat(actual)
-                .isNotNull()
-                .isEqualTo("Period{}");
-
-    }
-
-    @Test
-    public void toString03() throws Exception {
-        Period target = new Period();
-        String actual = target.toString();
-        assertThat(actual)
-                .isNotNull()
-                .isEqualTo("Period{}");
-
     }
 
 }
--- models/generic/src/main/java/org/venuspj/studio/generic/fundamentals/datetime/Period.java
+++ models/generic/src/main/java/org/venuspj/studio/generic/fundamentals/datetime/Period.java
@@ -1,7 +1,5 @@
 package org.venuspj.studio.generic.fundamentals.datetime;
 
-import org.venuspj.util.objects2.Objects2;
-
 public class Period {
     protected RecordDate startDate;
     protected RecordDate endDate;
@@ -26,13 +24,4 @@ public class Period {
         return endDate;
     }
 
-    @Override
-    public String toString() {
-        return Objects2
-                .toStringHelper(this)
-                .add("startDate", startDate)
-                .add("endDate", endDate)
-                .omitNullValues()
-                .toString();
-    }
 }
