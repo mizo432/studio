32c763f9c72cd9704a06556c3d8dcae98055d150
KenichiMizoguchi <hachirouuemon.mizoguchi@gmail.com>
2017-12-08 11:27:50 +0900

    [LHS-41-73]FindBugs対応

--- models/generic/src/main/java/org/venuspj/studio/generic/model/role/RoleInformation.java
+++ models/generic/src/main/java/org/venuspj/studio/generic/model/role/RoleInformation.java
@@ -3,9 +3,15 @@ package org.venuspj.studio.generic.model.role;
 import org.venuspj.studio.generic.fundamentals.name.Name;
 
 public class RoleInformation {
+
     private Name roleName;
 
     protected RoleInformation(Name aRoleName) {
         roleName = aRoleName;
     }
+
+    public Name getRoleName() {
+        return roleName;
+    }
+
 }
